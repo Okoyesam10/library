f3c07b74f6a64da9b349c7f4c1257e37c272874f
Sam Okoye <Okoyesam10@outlook.com>
2022-02-07 21:57:41 +0000

    init

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import library from './library'
+import router from './router'
+import './index.css'
 import App from './App.vue'
 
 createApp(App)
 .use(library)
 .mount('#app')
+.use(router)
